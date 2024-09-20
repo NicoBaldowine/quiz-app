821997dd7392586a82c52e23044c9aa65dca77bc
Nico Baldovino <nicobaldovino@Nicos-Laptop.local>
2024-09-20 14:41:00 -0600

    Fixed navigation for Create Quiz button

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 const HomeScreen = () => {
   return (
     <div>
       <h1>Available Quizzes</h1>
-      {/* Buttons */}
-      <button>Account</button>
-      <button>Create a Quiz</button>
+      {/* Navigation buttons */}
+      <Link to="/account">
+        <button>Account</button>
+      </Link>
+      <Link to="/create">
+        <button>Create a Quiz</button>
+      </Link>
 
       {/* Quiz cards or empty state */}
       <p>No quizzes available yet.</p>
