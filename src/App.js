8e9efb38725c071e72abc710ed875c2473aa0841
Nico Baldovino <nicobaldovino@Nicos-Laptop.local>
2024-09-21 10:11:45 -0600

    Fix: New home5

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { NextUIProvider } from '@nextui-org/react'; // Import NextUIProvider
 import HomeScreen from './screens/HomeScreen';
 import CreateQuizScreen from './screens/CreateQuizScreen';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomeScreen />} />
-        <Route path="/create" element={<CreateQuizScreen />} />
-      </Routes>
-    </Router>
+    <NextUIProvider> {/* Wrap your app with NextUIProvider */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/create" element={<CreateQuizScreen />} />
+        </Routes>
+      </Router>
+    </NextUIProvider>
   );
 }
 
